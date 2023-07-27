7a436a12100e245a823ce4a7c56068bdbda64187
samarth-kumar-05 <samarthkumar051102@gmail.com>
2023-07-27 14:52:28 +0530

    Seetings.py updated

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn all_api.wsgi --log-file -
+web: gunicorn all_api.wsgi
--- all_api/settings.py
+++ all_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!1p9v7u3$r&05w4x(w_^nga-i)iay4_t&is94s*f9cjqw+w3fr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -110,11 +110,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+import os
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-server-production-6d04.up.railway.app/'
+]
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
